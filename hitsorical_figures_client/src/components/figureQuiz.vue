aab77074a4aabed08933e6446f879fcaf29d947b
Daniel <danieldev0353@outlook.com>
2020-12-08 16:01:20 +0000

    colour coding for answer true or false work in progress. current colour impacts header as default red due to null default. development required

--- hitsorical_figures_client/src/components/figureQuiz.vue
+++ hitsorical_figures_client/src/components/figureQuiz.vue
@@ -3,30 +3,27 @@
       <h1>{{figure.name}} Quiz</h1>
       
       <form  v-on:submit.prevent="handleSubmit">
-        <h2> Q1 Is my birthday {{figure.born.date}}</h2>
+        <h2 > Q1 Is my birthday {{figure.born.date}}</h2>
         <input type="radio" class="question" name="question1" v-model="q1Answer" value="true">
-        <label for="true">TRUE</label><br>
+        <label for="true" :class="addClass(q1Answer)">TRUE</label><br>
         <input type="radio" class="question" name="question1" v-model="q1Answer" value="false">
-        <label for="false">FALSE</label><br>
-        <input type="submit" value="Submit">
+        <label for="false":class="addClass(q1Answer)">FALSE</label><br>
       </form>
 
       <form v-on:submit.prevent="handleSubmit">
         <h2> Q2 Was my job {{figure.occupation}}</h2>
-        <input type="radio" class="question" name="question2" value="true">
-        <label for="true">TRUE</label><br>
-        <input type="radio" class="question" name="question2" value="false">
-        <label for="false">FALSE</label><br>
-        <input type="submit" value="Submit">
+        <input type="radio" class="question" name="question2" value="true" v-model="q2Answer">
+        <label for="true" :class="addClass(q1Answer)">TRUE</label><br>
+        <input type="radio" class="question" name="question2" value="false" v-model="q2Answer">
+        <label for="false" :class="addClass(q1Answer)">FALSE</label><br>
       </form>
 
       <form v-on:submit.prevent="handleSubmit">
         <h2> Q3 Am I best known for {{figure.bestKnownFor}}</h2>
-        <input type="radio" class="question" name="question3" value="true">
-        <label for="true">TRUE</label><br>
-        <input type="radio" class="question" name="question3" value="false">
-        <label for="false">FALSE</label><br>
-        <input type="submit" value="Submit">
+        <input type="radio" class="question" name="question3" value="true" v-model="q3Answer">
+        <label for="true" :class="addClass(q1Answer)">TRUE</label><br>
+        <input type="radio" class="question" name="question3" value="false" v-model="q3Answer">
+        <label for="false":class="addClass(q1Answer)" >FALSE</label><br>
       </form>
   </div>    
 </template>
@@ -41,24 +38,34 @@ export default {
     return {
       q1Answer: null,
       q2Answer: null,
-      q3Answer: null,
-      
+      q3Answer: null
     }
   },
   props: ["figure", "historicalFigures"],
   methods: {
     handleSubmit(){
-      if (this.q1Answer == 'true') {
-        console.log("correct")
+      
+  // add computed class on submiting to add css green/red  
+    },
+    addClass(answer){
+      let className = ""
+      if (answer == 'true'){
+        className = 'true'
+      } else {
+        className = 'false'
       }
-      
-  // add computed class on submiting to add css green/red 
-      
+      return className 
     }
+   
   }
 }
 </script>
 
 <style scoped>
-
+.true {
+  color: green;
+}
+.false {
+  color: red;
+}
 </style>
