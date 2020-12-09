7b75bcb266907e19544bc4c9ed3ca2755ba93988
Daniel <danieldev0353@outlook.com>
2020-12-09 12:17:38 +0000

    Added computed properties to display feedback

--- hitsorical_figures_client/src/components/figureQuiz.vue
+++ hitsorical_figures_client/src/components/figureQuiz.vue
@@ -6,28 +6,27 @@
         <h2 > Q1 Is my birthday {{figure.born.date}}</h2>
         <input type="radio" class="question" name="question1" v-model="q1Answer" value="yes">
         <label for="true" >YES</label><br>
-        <input type="radio" class="question" name="question1" v-model="q1Answer" value="no">
+        <input type="radio"  name="question1" v-model="q1Answer" value="no">
         <label for="false">NO</label><br>
-        <p :class="addClass(q1Answer)">Correct</p>
+        <p :class="displayFeedback1">{{correctFeedback1}}</p>
       </form>
 
       <form v-on:submit.prevent="handleSubmit">
         <h2> Q2 Was my job {{figure.occupation}}</h2>
         <input type="radio" class="question" name="question2" value="yes" v-model="q2Answer">
         <label for="true" >YES</label><br>
-        <input type="radio" class="question" name="question2" value="no" v-model="q2Answer">
+        <input type="radio"  name="question2" value="no" v-model="q2Answer">
         <label for="false">NO</label><br>
-        <p :class="addClass(q2Answer)">Correct</p>
+        <p :class="displayFeedback2">{{correctFeedback2}}</p>
       </form>
 
       <form v-on:submit.prevent="handleSubmit">
         <h2> Q3 Am I best known for {{figure.bestKnownFor}}</h2>
-        <input type="radio" class="question" name="question3" value="yes" v-model="q3Answer">
+        <input type="radio"  name="question3" value="yes" v-model="q3Answer">
         <label for="true" >YES</label><br>
-        <input type="radio" class="question" name="question3" value="no" v-model="q3Answer">
+        <input type="radio"  name="question3" value="no" v-model="q3Answer">
         <label for="false">NO</label><br>
-        <p :class="addClass(q3Answer)">Correct</p>
-        <p>Incorrect</p>
+        <p :class="displayFeedback3">{{correctFeedback3}}</p>
       </form>
   </div>    
 </template>
@@ -42,30 +41,66 @@ export default {
     return {
       q1Answer: null,
       q2Answer: null,
-      q3Answer: null
+      q3Answer: null,
     }
   },
   props: ["figure", "historicalFigures"],
+  computed:{
+    correctFeedback1(){
+      let feedbackMessage = ""
+      if (this.q1Answer === 'yes'){
+        feedbackMessage = 'Correct'
+        } else {
+        feedbackMessage = 'Incorrect'
+      }
+      return feedbackMessage
+    },
+    correctFeedback2(){
+      let feedbackMessage = ""
+      if (this.q2Answer === 'yes'){
+        feedbackMessage = 'Correct'
+        } else {
+        feedbackMessage = 'Incorrect'
+      }
+      return feedbackMessage
+    },
+
+    correctFeedback3(){
+      let feedbackMessage = ""
+      if (this.q3Answer === 'yes'){
+        feedbackMessage = 'Correct'
+        } else {
+        feedbackMessage = 'Incorrect'
+      }
+      return feedbackMessage
+    },
+
+    displayFeedback1(){
+      return this.q1Answer ? "visible" : "invisible"
+    },
+
+     displayFeedback2(){
+      return this.q2Answer ? "visible" : "invisible"
+    },
+
+     displayFeedback3(){
+      return this.q3Answer ? "visible" : "invisible"
+    }
+  },
   methods: {
-    
-    addClass(answer){
-      let className = ""
-      if (answer == 'yes'){
-        className = 'true'
-      
-      return className 
-    }
-    }
-  }
+}
 }
 </script>
 
 <style scoped>
-form p {
+ .invisible {
   display: none;
 }
-.true {
+.visible {
   display: initial;
 }
+
+
+
 
 </style>
